73897d5a1bc77f74efd08fc9be3b558b25a525eb
Vishal Kulkarni <vkvishal1508@gmail.com>
2024-04-30 12:39:48 -0400

    JS: String Casing Updated

--- 14 Introduction to Javascript/String_Casing.js
+++ 14 Introduction to Javascript/String_Casing.js
@@ -1,2 +1,2 @@
 var name = prompt("What is your name?");
-alert("Hello" + name.slice(0, 1).toUpperCase() + name.slice(1).toLowerCase());
+alert("Hello, " + name.slice(0, 1).toUpperCase() + name.slice(1).toLowerCase());
