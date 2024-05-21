e5a5e64d8f0faf2c2f7ae80a9214491b3840ba33
Vishal Kulkarni <vkvishal1508@gmail.com>
2024-05-21 12:34:10 -0400

    Updated Leap Year Check

--- 14 Introduction to Javascript/Leap_Year.js
+++ 14 Introduction to Javascript/Leap_Year.js
@@ -2,7 +2,7 @@ function isLeap(year) {
 	/**************Don't change the code above****************/
 
 	//Write your code here.
-	if (year % 4 == 0 || (year % 100 != 0 && year % 400 == 0)) {
+	if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
 		return "Leap year.";
 	} else {
 		return "Not leap year.";
