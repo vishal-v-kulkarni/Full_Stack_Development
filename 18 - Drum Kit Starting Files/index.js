85fbaf993e3d3b69de0aaf0a11a0e3fed8e1e2bf
Vishal Kulkarni <vkvishal1508@gmail.com>
2024-06-04 11:19:04 -0400

    Higher Order Functions and Passing Function as an Argument - Index.js file

--- 18 - Drum Kit Starting Files/index.js
+++ 18 - Drum Kit Starting Files/index.js
@@ -5,3 +5,23 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
 		alert("I got clicked!");
 	});
 }
+
+function add(num1, num2) {
+	return num1 + num2;
+}
+
+function subtract(num1, num2) {
+	return num1 - num2;
+}
+
+function multiply(num1, num2) {
+	return num1 * num2;
+}
+
+function divide(num1, num2) {
+	return num1 / num2;
+}
+
+function calculator(num1, num2, operator) {
+	return operator(num1, num2);
+}
