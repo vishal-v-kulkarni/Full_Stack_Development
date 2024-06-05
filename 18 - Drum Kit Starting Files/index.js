3b831151cf4eb26ab9d333d21a4adf59f41dcf06
Vishal Kulkarni <vkvishal1508@gmail.com>
2024-06-05 00:01:34 -0400

    Drum Kit Sounds - Intermediate JS

--- 18 - Drum Kit Starting Files/index.js
+++ 18 - Drum Kit Starting Files/index.js
@@ -2,26 +2,45 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfDrumButtons; i++) {
 	document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-		alert("I got clicked!");
+		var drumName = this.innerHTML;
+		var audioName = "";
+		if (drumName === "w") {
+			audioName = "tom-1.mp3";
+		} else if (drumName === "a") {
+			audioName = "tom-2.mp3";
+		} else if (drumName === "s") {
+			audioName = "tom-3.mp3";
+		} else if (drumName === "d") {
+			audioName = "tom-4.mp3";
+		} else if (drumName === "j") {
+			audioName = "snare.mp3";
+		} else if (drumName === "k") {
+			audioName = "crash.mp3";
+		} else {
+			audioName = "kick-bass.mp3";
+		}
+
+		var audio = new Audio("./sounds/" + audioName);
+		audio.play();
 	});
 }
 
-function add(num1, num2) {
-	return num1 + num2;
-}
+// function add(num1, num2) {
+// 	return num1 + num2;
+// }
 
-function subtract(num1, num2) {
-	return num1 - num2;
-}
+// function subtract(num1, num2) {
+// 	return num1 - num2;
+// }
 
-function multiply(num1, num2) {
-	return num1 * num2;
-}
+// function multiply(num1, num2) {
+// 	return num1 * num2;
+// }
 
-function divide(num1, num2) {
-	return num1 / num2;
-}
+// function divide(num1, num2) {
+// 	return num1 / num2;
+// }
 
-function calculator(num1, num2, operator) {
-	return operator(num1, num2);
-}
+// function calculator(num1, num2, operator) {
+// 	return operator(num1, num2);
+// }
