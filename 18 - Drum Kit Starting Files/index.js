2e3ab336bb3df3a36f4e8bb38442bce25b9c32af
Vishal Kulkarni <vkvishal1508@gmail.com>
2024-06-05 12:58:50 -0400

    Switch statements in JS

--- 18 - Drum Kit Starting Files/index.js
+++ 18 - Drum Kit Starting Files/index.js
@@ -4,20 +4,27 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
 	document.querySelectorAll(".drum")[i].addEventListener("click", function () {
 		var drumName = this.innerHTML;
 		var audioName = "";
-		if (drumName === "w") {
-			audioName = "tom-1.mp3";
-		} else if (drumName === "a") {
-			audioName = "tom-2.mp3";
-		} else if (drumName === "s") {
-			audioName = "tom-3.mp3";
-		} else if (drumName === "d") {
-			audioName = "tom-4.mp3";
-		} else if (drumName === "j") {
-			audioName = "snare.mp3";
-		} else if (drumName === "k") {
-			audioName = "crash.mp3";
-		} else {
-			audioName = "kick-bass.mp3";
+		switch (drumName) {
+			case "w":
+				audioName = "tom-1.mp3";
+				break;
+			case "a":
+				audioName = "tom-2.mp3";
+				break;
+			case "s":
+				audioName = "tom-3.mp3";
+				break;
+			case "d":
+				audioName = "tom-4.mp3";
+				break;
+			case "j":
+				audioName = "snare.mp3";
+				break;
+			case "k":
+				audioName = "crash.mp3";
+				break;
+			default:
+				audioName = "kick-bass.mp3";
 		}
 
 		var audio = new Audio("./sounds/" + audioName);
