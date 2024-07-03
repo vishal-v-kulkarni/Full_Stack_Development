d4a2ff24ace52b2a38bc77c825cda8a51e01783a
Vishal Kulkarni <vkvishal1508@gmail.com>
2024-07-02 23:32:58 -0400

    HTTP Requests - GET and Nodemon with Global Installation

---
+++ Backend/3.2 HTTP Request/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+	res.send("<h1>Hello, world!</h1>");
+});
+
+app.listen(port, () => {
+	console.log("The server is running on Port " + port);
+});
