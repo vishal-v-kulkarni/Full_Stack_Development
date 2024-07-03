3ec34d0c2c9e60e734a9558b2d8791610d2249f8
Vishal Kulkarni <vkvishal1508@gmail.com>
2024-07-03 14:41:10 -0400

    HTTP GET Request with Different Endpoints

--- Backend/3.2 HTTP Request/index.js
+++ Backend/3.2 HTTP Request/index.js
@@ -6,6 +6,14 @@ app.get("/", (req, res) => {
 	res.send("<h1>Hello, world!</h1>");
 });
 
+app.get("/contact", (req, res) => {
+	res.send("<h1>Contact Us</h1>");
+});
+
+app.get("/about", (req, res) => {
+	res.send("<h1>About Me</h1>");
+});
+
 app.listen(port, () => {
 	console.log("The server is running on Port " + port);
 });
