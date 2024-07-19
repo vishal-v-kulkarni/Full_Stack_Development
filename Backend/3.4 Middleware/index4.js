04503120f7ce2399afde6db71aa02254b221ec30
Vishal Kulkarni <vkvishal1508@gmail.com>
2024-07-19 13:18:34 -0400

    Custom Middleware in the Bandname Generator Challenge

--- Backend/3.4 Middleware/index4.js
+++ Backend/3.4 Middleware/index4.js
@@ -6,8 +6,18 @@ import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
+var bandName = "";
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
+//Use this custom middleware to add the bandname
+function bandNameGenerator(req, res, next) {
+	console.log(req.body);
+	bandName = req.body["street"] + req.body["pet"];
+	next();
+}
+
+app.use(bandNameGenerator);
 
 app.get("/", (req, res) => {
 	res.sendFile(__dirname + "/public/index.html");
