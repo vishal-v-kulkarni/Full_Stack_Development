60f0cf0df085e5a4213bfe446126314fab15e87b
Vishal Kulkarni <vkvishal1508@gmail.com>
2024-07-19 12:55:56 -0400

    Custom Middleware

--- Backend/3.4 Middleware/index3.js
+++ Backend/3.4 Middleware/index3.js
@@ -3,12 +3,19 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+//Function for logging and use this in the middleware
+function logger(req, res, next) {
+	console.log("Request method", req.method + req.url);
+	next(); //This is very important. Without this it will not exit the middleware.
+}
+
+//Custom middleware using the function above
 app.use(logger);
 
 app.get("/", (req, res) => {
-  res.send("Hello");
+	res.send("Hello");
 });
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+	console.log(`Listening on port ${port}`);
 });
