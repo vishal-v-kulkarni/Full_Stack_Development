fcc38ddfd1f8af47d9a5b768108550f3d9e7972b
Vishal Kulkarni <vkvishal1508@gmail.com>
2024-08-28 17:06:31 -0400

    Challenge - Server API request - Part 2: Post request from Frontend to Server to Public API

--- Backend/5.3 Axios/index.js
+++ Backend/5.3 Axios/index.js
@@ -15,7 +15,7 @@ app.get("/", async (req, res) => {
 	try {
 		const response = await axios.get("https://bored-api.appbrewery.com/random");
 		const result = response.data;
-		console.log(result);
+		//console.log(result);
 		res.render("index.ejs", { data: result });
 	} catch (error) {
 		console.error("Failed to make request:", error.message);
@@ -27,6 +27,23 @@ app.get("/", async (req, res) => {
 
 app.post("/", async (req, res) => {
 	console.log(req.body);
+	const type = req.body.type;
+	const participants = req.body.participants;
+
+	try {
+		const response = await axios.get(
+			`https://bored-api.appbrewery.com/filter?type=${type}&participants=${participants}`
+		);
+		const result =
+			response.data[Math.floor(Math.random() * response.data.length)];
+		//console.log(result);
+		res.render("index.ejs", { data: result });
+	} catch (error) {
+		console.error("Failed to make request:", error.message);
+		res.render("index.ejs", {
+			error: error.message,
+		});
+	}
 
 	// Step 2: Play around with the drop downs and see what gets logged.
 	// Use axios to make an API request to the /filter endpoint. Making
