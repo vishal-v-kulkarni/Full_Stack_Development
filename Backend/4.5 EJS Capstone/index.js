99fb285f5e469f68745c086b1c885c509a94dda6
Vishal Kulkarni <vkvishal1508@gmail.com>
2024-09-04 16:35:09 -0400

    4.5 EJS Capstone - Express Static added

--- Backend/4.5 EJS Capstone/index.js
+++ Backend/4.5 EJS Capstone/index.js
@@ -5,6 +5,7 @@ const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.listen(port, () => {
 	console.log(`Node Server running on port ${port}`);
