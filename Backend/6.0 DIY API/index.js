ff6cf084d919fbbf007205607d262bfbfb2f16ba
Vishal Kulkarni <vkvishal1508@gmail.com>
2024-09-13 14:25:54 -0400

    DYI API - POST, PUT, and PATCH requests

--- Backend/6.0 DIY API/index.js
+++ Backend/6.0 DIY API/index.js
@@ -30,10 +30,52 @@ app.get("/filter", (req, res) => {
 });
 
 //4. POST a new joke
+app.post("/jokes", (req, res) => {
+	const newJoke = {
+		id: jokes.length + 1,
+		jokeText: req.body.text,
+		jokeType: req.body.type,
+	};
+
+	jokes.push(newJoke);
+	console.log(jokes.slice(-1));
+	res.json(newJoke);
+});
 
 //5. PUT a joke
+app.put("/jokes/:id", (req, res) => {
+	const id = parseInt(req.params.id);
+	var joke = jokes.find((joke) => joke.id === id);
+	joke.jokeText = req.body.text;
+	joke.jokeType = req.body.type;
+
+	console.log(joke);
+	res.json(joke);
+
+	//Alternate logic
+	// const replacementJoke = {
+	// 	id: id,
+	// 	jokeText: req.body.text,
+	// 	jokeType: req.body.type,
+	// };
+
+	// const searchIndex = jokes.findIndex((joke) => joke.id === id);
+	// jokes[searchIndex] = replacementJoke;
+	// res.json(replacementJoke);
+});
 
 //6. PATCH a joke
+app.patch("/jokes/:id", (req, res) => {
+	const id = parseInt(req.params.id);
+	var existingJoke = jokes.find((joke) => joke.id === id);
+
+	existingJoke.jokeText = req.body.text || existingJoke.jokeText;
+	existingJoke.jokeType = req.body.type || existingJoke.jokeType;
+
+	//Alternate logic as above in PUT
+	console.log(existingJoke);
+	res.send(existingJoke);
+});
 
 //7. DELETE Specific joke
 
